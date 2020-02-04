88e1bb0a7771ac4bdcb6b91b351aabd5eb293c7f
sistemperpustakaan <60570646+sistemperpustakaan@users.noreply.github.com>
2020-02-04 10:04:09 +0700

    Add files via upload

--- db_perpustakaan.sql
+++ db_perpustakaan.sql
@@ -1,20 +1,22 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.7.1
--- http://www.phpmyadmin.net
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 30, 2020 at 07:17 AM
--- Server version: 5.6.20
--- PHP Version: 5.5.15
+-- Generation Time: 04 Feb 2020 pada 03.56
+-- Versi Server: 10.1.24-MariaDB
+-- PHP Version: 7.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `db_perpustakaan`
@@ -23,60 +25,61 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `daftar_buku`
+-- Struktur dari tabel `daftar_buku`
 --
 
-CREATE TABLE IF NOT EXISTS `daftar_buku` (
-`id` int(5) NOT NULL,
+CREATE TABLE `daftar_buku` (
+  `id` int(5) NOT NULL,
   `no_buku` varchar(100) DEFAULT NULL,
   `judul_buku` varchar(100) DEFAULT NULL,
   `penerbit` varchar(100) DEFAULT NULL,
   `tahun_terbit` varchar(100) DEFAULT NULL,
   `pengarang` varchar(100) DEFAULT NULL,
-  `ruang_buku` varchar(100) DEFAULT NULL,
   `status` varchar(100) DEFAULT NULL,
-  `ket` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=14 ;
+  `ket` varchar(100) DEFAULT NULL,
+  `ruang_buku` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `daftar_buku`
+-- Dumping data untuk tabel `daftar_buku`
 --
 
-INSERT INTO `daftar_buku` (`id`, `no_buku`, `judul_buku`, `penerbit`, `tahun_terbit`, `pengarang`, `ruang_buku`, `status`, `ket`) VALUES
-(13, '01', 'Belajar Corel Draw', 'Gramedia', '2018', 'Sutan', NULL, NULL, NULL);
+INSERT INTO `daftar_buku` (`id`, `no_buku`, `judul_buku`, `penerbit`, `tahun_terbit`, `pengarang`, `status`, `ket`, `ruang_buku`) VALUES
+(28, '99', 'Menjadi Hokage', 'M.Rizky Pratama', '2020', 'M.Rizky Pratama', 'Ada', 'Menjadi Hokage terhebat yang pernah ada', 'Data Peminjam'),
+(29, '98', 'Good News', 'Henry', '2008', 'Michael', 'Ada', 'Pembaharuan', 'ruang_buku');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `peminjaman`
+-- Struktur dari tabel `peminjaman`
 --
 
-CREATE TABLE IF NOT EXISTS `peminjaman` (
-  `no_perkara` varchar(50) NOT NULL,
+CREATE TABLE `peminjaman` (
+  `no_peminjam` varchar(50) NOT NULL,
   `peminjam` varchar(50) NOT NULL,
   `tgl_pinjam` varchar(25) NOT NULL,
   `tgl_kembali` varchar(25) NOT NULL,
   `lama_pinjam` int(5) NOT NULL,
   `keterangan` varchar(100) NOT NULL,
-`id` int(5) NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+  `id` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `peminjaman`
+-- Dumping data untuk tabel `peminjaman`
 --
 
-INSERT INTO `peminjaman` (`no_perkara`, `peminjam`, `tgl_pinjam`, `tgl_kembali`, `lama_pinjam`, `keterangan`, `id`) VALUES
-('01/PID.B/2018/PN.KIS', 'A. Nasution', '2018-12-14', '2018-12-16', 2, '-', 4),
-('01/PDT.G/2018/PN.KIS', 'J. Siahaan', '2018-12-10', 'Belum Kembali', 0, '-', 5),
-('02/PID.B/2018/PN.KIS', 'Bejo', '2018-12-11', 'Belum Kembali', 0, 'Input ke CTS', 6);
+INSERT INTO `peminjaman` (`no_peminjam`, `peminjam`, `tgl_pinjam`, `tgl_kembali`, `lama_pinjam`, `keterangan`, `id`) VALUES
+('01/PID.B/2018/PN.KIS', 'Adam', '2017-12-31', '2017-12-31', 0, '-', 4),
+('01/PDT.G/2018/PN.KIS', 'Judi', '2017-12-31', '2018-02-16', 45, '-', 5),
+('02/PID.B/2018/PN.KIS', 'Bejo', '2018-12-11', '2020-02-03', 412, 'Input ke CTS', 6);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Struktur dari tabel `user`
 --
 
-CREATE TABLE IF NOT EXISTS `user` (
+CREATE TABLE `user` (
   `username` varchar(25) NOT NULL,
   `paswd` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
@@ -86,7 +89,7 @@ CREATE TABLE IF NOT EXISTS `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `user`
+-- Dumping data untuk tabel `user`
 --
 
 INSERT INTO `user` (`username`, `paswd`, `email`, `nama`, `level`, `ket`) VALUES
@@ -100,19 +103,19 @@ INSERT INTO `user` (`username`, `paswd`, `email`, `nama`, `level`, `ket`) VALUES
 -- Indexes for table `daftar_buku`
 --
 ALTER TABLE `daftar_buku`
- ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `peminjaman`
 --
 ALTER TABLE `peminjaman`
- ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
- ADD PRIMARY KEY (`username`);
+  ADD PRIMARY KEY (`username`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -122,12 +125,13 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `daftar_buku`
 --
 ALTER TABLE `daftar_buku`
-MODIFY `id` int(5) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=14;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 --
 -- AUTO_INCREMENT for table `peminjaman`
 --
 ALTER TABLE `peminjaman`
-MODIFY `id` int(5) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
